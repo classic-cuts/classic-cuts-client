dbb10f008e1f855bbfb3324efe417d8f509f4904
Rajneesh Mishra <rajneesh.solulab@gmail.com>
2023-09-12 14:09:13 +0530

    Revert "tailwind config"
    This reverts commit 0354d3a0116d9a402f3a7b4817af5084c374ab54.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports =  {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        bodyfont: ["Poppins", "sans-serif"],
-        titlefont: ["Monsterrat", "sans-serif"],
-      },
-    },
-  },
-  plugins: [
-    require('tailwind-scrollbar'),
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-};
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

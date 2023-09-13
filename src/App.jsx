c9fe87e49df221a18e318d273613c1a058e03052
Faizan Mansuri <126244804+DevFaizann@users.noreply.github.com>
2023-09-13 20:45:06 +0530

    created products component | newsletter form WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Homepage from './pages/Homepage'
-import Footer from './components/Footer'
+import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Footer from "./components/footer/Footer";
+import ProductsList from "./components/products-list/ProductsList";
+import { FrequentBuys } from "./components/products-list/FrequentBuys";
 
-const App = ()=> {
+const ScrollToTop = () => {
+  const {pathname} = useLocation();
 
-
-  return (
-    <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
-    </Router>
-    <Footer/>
-    </>
-  )
+  useEffect(() => {
+    window.scrollTo(0,0);
+  }, [pathname])
+  
+  return null;
 }
+const App = () => {
+  return (
+    <div className="app">
+      <Router>
+        <ScrollToTop/>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </Router>
+      <ProductsList/>
+      <FrequentBuys/>
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;

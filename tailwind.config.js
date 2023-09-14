bb31a0f9cdda6df14f5b51e3d392e8d6097f03b6
Rajneesh Mishra <rajneesh.solulab@gmail.com>
2023-09-14 15:32:55 +0530

    fix input box

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,20 @@
 /** @type {import('tailwindcss').Config} */
 
 // eslint-disable-next-line no-undef
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 // eslint-disable-next-line no-undef
-module.exports =  {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       boxShadow: {
-        'custom': '0 4px 9px -4px #3b71ca',
-        'custom2': '0 8px 9px -4px rgba(59, 113, 202, 0.3), 0 4px 18px 0 rgba(59, 113, 202, 0.2)',
+        custom: "0 4px 9px -4px #3b71ca",
+        custom2:
+          "0 8px 9px -4px rgba(59, 113, 202, 0.3), 0 4px 18px 0 rgba(59, 113, 202, 0.2)",
       },
       gridTemplateRows: {
-        '[auto,auto,1fr]': 'auto auto 1fr',
+        "[auto,auto,1fr]": "auto auto 1fr",
       },
       colors: {
         // eslint-disable-next-line no-undef
@@ -26,16 +27,16 @@ module.exports =  {
         bodyfont: ["Poppins", "sans-serif"],
         titlefont: ["Monsterrat", "sans-serif"],
       },
-      container:{
+      container: {
         center: true,
         padding: "150px",
-      }
+      },
     },
   },
   plugins: [
     // eslint-disable-next-line no-undef
-    require('tailwind-scrollbar'),
-    require('@tailwindcss/aspect-ratio'),
-    
+    require("tailwind-scrollbar"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
   ],
 };

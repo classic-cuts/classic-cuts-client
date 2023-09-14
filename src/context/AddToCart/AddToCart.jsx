8e53bf7d3caa26061e0e818ed08c01d76e251934
Faizan Mansuri <126244804+DevFaizann@users.noreply.github.com>
2023-09-14 20:58:22 +0530

    contextAPI to get products

--- src/context/AddToCart/AddToCart.jsx
+++ src/context/AddToCart/AddToCart.jsx
@@ -1,7 +1,49 @@
-const AddToCart = () => {
-  return (
-    <div>AddToCart</div>
-  )
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+import axios from "axios";
+import { useContext, createContext, useEffect, useReducer } from "react";
+import reducer from "../../reducers/ProductReducer"
+
+const AppContext = createContext();
+
+const API = "https://mocki.io/v1/e655d260-1567-4520-b15f-a7e318e634bb"
+
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  featureProducts: [],
 }
 
-export default AddToCart
+const AppProvider = ({ children }) => {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const getProducts = async (url) => {
+    dispatch({ type:"SET_LOADING"});
+      try {
+        const res = await axios.get(url)
+        const products = await res.data;
+        dispatch({type: "SET_API_DATA", payload: products});
+      } catch (error) {
+        dispatch({type:"API_ERROR"});
+      }
+      
+  };
+
+  useEffect(() => {
+    getProducts(API);
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ ...state }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+const useProductContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider, AppContext, useProductContext };
---
+++ src/reducers/ProductReducer.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable no-fallthrough */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-case-declarations */
+/* eslint-disable no-undef */
+const ProductReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_LOADING":
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case "SET_API_DATA":
+      const featureData = action.payload.filter((curElem) => {
+        return curElem.featured === true;
+      });
+      return {
+        ...state,
+        isLoading: false,
+        products: action.payload,
+        featureProducts: featureData,
+      };
+
+    case "API_ERROR":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default ProductReducer;
